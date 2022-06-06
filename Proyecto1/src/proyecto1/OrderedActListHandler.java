be447796ebd7de9f43908514d904aba255474502
Felipe Arias <52615972+PhilArias@users.noreply.github.com>
2022-06-06 05:17:24 -0500

    Add files via upload

--- Proyecto1/src/proyecto1/OrderedActListHandler.java
+++ Proyecto1/src/proyecto1/OrderedActListHandler.java
@@ -1,18 +1,24 @@
-package Proyecto1;
+package com.Sesion;
 
 public class OrderedActListHandler<Item> {
     
+    
     public void order(List<Item> list) {
-        Node<Item> selected = list.head;
-        while(selected.next != null && selected != null ){
-            Node<Item> iter = selected.next;
-            while(iter != null){
-                if(((Activity)iter.getValue()).compare((Activity)iter.getValue(), (Activity)selected.getValue()) < 0){
-                    list.swap(selected, iter);
+
+        if (list.size > 0) {
+            Node<Item> selected = list.head;
+            while (selected.next != null && selected != null) {
+                Node<Item> iter = selected.next;
+                while (iter != null) {
+                    if (((Activity) iter.getValue()).compare((Activity) iter.getValue(), (Activity) selected.getValue()) < 0) {
+                        list.swap(selected, iter);
+                    }
+                    iter = iter.next;
                 }
-                iter = iter.next;
+                selected = selected.next;
             }
-            selected = selected.next;
-        } 
+
+        }
+
     }
 }
